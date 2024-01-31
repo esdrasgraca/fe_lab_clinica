02320854a1f75e04a319d444cce06b3302f6a151
esdrasgraca <esdras.esilva@gmail.com>
2024-01-31 01:46:18 -0300

    Criado o projeto do self Service e concluído a aula 2 do dia 2

---
+++ fe_lab_clinicas_self_service/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service/src/binding/lab_clinicas_application_binding.dart';
+import 'package:fe_lab_clinicas_self_service/src/pages/splash_page/splash_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+void main() {
+  runApp(const LavClinicasSelfServiceApp());
+}
+
+class LavClinicasSelfServiceApp extends StatelessWidget {
+  const LavClinicasSelfServiceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LabClinicasCoreConfig(
+      tittle: 'Lab Clinicas Auto Atendimento',
+      bindings: LabClinicasApplicationBinding(),
+      pagesBuilders: [
+        FlutterGetItPageBuilder(
+          page: (_) => const SplashPage(),
+          path: '/',
+        )
+      ],
+    );
+  }
+}
---
+++ fe_lab_clinicas_self_service/lib/src/core/env.dart
@@ -0,0 +1,4 @@
+
+final class Env {
+  static const backendBaseURL = String.fromEnvironment('BACKEND_BASE_URL');
+}
